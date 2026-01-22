cd35b2c24de0477aad9cf49089280451ab0a3a1e
T.Srikar Prasad <srikarterala1123@gmail.com>
2026-01-22 21:27:01 +0530

    Added basic REST controller

---
+++ src/main/java/com/example/demoday1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demoday1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from REST Controller";
+    }
+}
